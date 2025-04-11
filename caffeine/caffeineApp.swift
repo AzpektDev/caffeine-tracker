8e7e25726c11f990b512bd2ab36e8ab981a2cb81
Azpekt <65199167+AzpektDev@users.noreply.github.com>
2025-04-11 11:06:43 +0200

    Initial Commit

---
+++ caffeine/caffeineApp.swift
@@ -0,0 +1,17 @@
+//
+//  caffeineApp.swift
+//  caffeine
+//
+//  Created by Eryk Janiczek on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct caffeineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ caffeineTests/caffeineTests.swift
@@ -0,0 +1,17 @@
+//
+//  caffeineTests.swift
+//  caffeineTests
+//
+//  Created by Eryk Janiczek on 4/11/25.
+//
+
+import Testing
+@testable import caffeine
+
+struct caffeineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
